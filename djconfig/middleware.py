58cdf713745c535b98cf5084d647ede1b494f67e
nitely <ochdownloader@gmail.com>
2014-04-27 17:02:44 -0300

    middleware backend check

--- djconfig/middleware.py
+++ djconfig/middleware.py
@@ -1,8 +1,14 @@
 #-*- coding: utf-8 -*-
 
+from django.conf import settings
+
 import djconfig
+from djconfig.settings import DJC_BACKEND
 
 __all__ = ['DjConfigLocMemMiddleware', ]
+
+
+BACKEND = getattr(settings, 'DJC_BACKEND', DJC_BACKEND)
 
 
 class DjConfigLocMemMiddleware(object):
@@ -11,4 +17,11 @@ class DjConfigLocMemMiddleware(object):
     required by the LocalMem backend
     """
     def process_request(self, request):
+        self.check_backend()
         djconfig.load()
+
+    def check_backend(self):
+        backend = settings.CACHES[BACKEND]
+
+        if not backend['BACKEND'].endswith(".LocMemCache"):
+            raise ValueError("DjConfigLocMemMiddleware requires LocMemCache as cache")
