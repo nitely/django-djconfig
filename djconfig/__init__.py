14ac2bed2fbbe3765a0e8ded98321daf337d2e4c
nitely <ochdownloader@gmail.com>
2014-08-18 02:32:46 -0300

    bump v0.1.5

--- djconfig/__init__.py
+++ djconfig/__init__.py
@@ -8,7 +8,7 @@ from djconfig.config import config, prefixer
 from djconfig.models import Config as ConfigModel
 from djconfig.settings import BACKEND, PREFIX
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = ['config', 'register']
 
 _registered_forms = set()
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djconfig',
-    version='0.1.4',
+    version='0.1.5',
     description='DjConfig is a Django app for storing other apps configurations.',
     author='Esteban Castro Borsani',
     author_email='ecastroborsani@gmail.com',
