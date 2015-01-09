1be419b91166700ab16cf8f87fa0848c26014495
nitely <ochdownloader@gmail.com>
2015-01-08 23:24:33 -0300

    bump 0.2.0

--- djconfig/__init__.py
+++ djconfig/__init__.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 
 from .conf import config
 from .registry import register
-from .settings import BACKEND
 
-__version__ = "0.1.8"
-__all__ = ['config', 'register', 'BACKEND']
+__version__ = "0.2.0"
+__all__ = ['config', 'register']
