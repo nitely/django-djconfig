b299c1d2e37d44bbe93b92350f60eed72cb4b842
nitely <ecastroborsani@gmail.com>
2016-11-13 22:03:25 -0300

    easier setup
    * Makefile commands
    * setup: get version from package
    * editorconfig
    * manifest

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
 import os
 from setuptools import setup, find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+BASE_DIR = os.path.join(os.path.dirname(__file__))
+URL = 'https://github.com/nitely/django-djconfig'
+README = "For more info, go to: {}".format(URL)
+
+
+def get_version(package):
+    """Get version without importing the lib"""
+    with io.open(os.path.join(BASE_DIR, package, '__init__.py'), encoding='utf-8') as fh:
+        return [
+            l.split('=', 1)[1].strip().strip("'")
+            for l in fh.readlines()
+            if '__version__' in l][0]
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djconfig',
-    version='0.5.0',
+    version=get_version('djconfig'),
     description='DjConfig is a Django app for storing dynamic configurations.',
     author='Esteban Castro Borsani',
     author_email='ecastroborsani@gmail.com',
     long_description=README,
-    url='https://github.com/nitely/django-djconfig',
-    packages=find_packages(exclude=['example', ]),
+    url=URL,
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     license='MIT License',
